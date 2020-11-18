3bb44e39f3ef5a878a84177fc87ff3da60aab2c1
Gina Sprint <sprint@gonzaga.edu>
2020-11-18 12:10:18 -0800

    start of notes

--- CoreLocationFunS1/ViewController.swift
+++ CoreLocationFunS1/ViewController.swift
@@ -6,8 +6,21 @@
 //
 
 import UIKit
+import CoreLocation
+
+// MARK: - Location Services
+// Simulator -> Features -> Location
+// set a predefined location/route or a custom location
+// CLLocationManager: a class used to get the user's location and updates
+// CLGeocoder: a class used to convert address/place -> coordinates and coordinates -> address/place
+// task: set up a UI with three labels: latitude, longitude, name
+// set up the outlets as well
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
